d0ec38d42ed9c8e41a34be9f8854bc78bfefb5a4
Charles Annibal <charlesannibal@gmail.com>
2018-04-09 17:24:54 -0300

    trying make the creature stand up and stable with gravity.

---
+++ Assets/Scripts/Movement.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		//this.GetComponent<Rigidbody>().AddForce(Vector3.down * 20000f * this.GetComponent<Rigidbody>().mass);
+		/*float translation = Input.GetAxis("Vertical") * speed;
+        float rotation = Input.GetAxis("Horizontal") * rotationSpeed;
+
+		
+
+		if (Input.GetButtonDown("Horizontal")){
+			float translation = Input.GetAxis("Horizontal") * speed;
+			this.transform.position =  new Vector3(this.transform.position.x - 1000f,this.transform.position.y,this.transform.position.z);
+		}
+
+		if (Input.GetButtonDown("Vertical")){
+			float translation = Input.GetAxis("Vertical") * speed;
+			this.transform.position =  new Vector3(this.transform.position.x - 1000f,this.transform.position.y,this.transform.position.z);
+		}*/
+
+	}
+}
