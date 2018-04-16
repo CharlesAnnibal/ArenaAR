8245fa90ed6f3277f0381b5b5682d8df46f506bb
Charles Annibal <charlesannibal@gmail.com>
2018-04-16 20:09:28 -0300

    Tentando fazer a mivimentacao funcionar

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -5,7 +5,9 @@ using UnityEngine;
 public class Movement : MonoBehaviour {
 
 	public float speed;
+	protected float r;
 	Vector3 movement;
+	Vector3 rotation;
 	Rigidbody playerRigidbody;
 	// Use this for initialization
 	void Awake(){
@@ -25,15 +27,67 @@ public class Movement : MonoBehaviour {
 		float horizontal = Input.GetAxisRaw("Horizontal");
 		float vertical  = Input.GetAxisRaw("Vertical");
 		Move(horizontal,vertical);
+		Debug.Log("Horizontal:"+horizontal);
+		Debug.Log("vertical:"+vertical);
+		Turning(horizontal,vertical);
 	}
 
 	void Move(float h,float v){
 		movement.Set(h,0f,v);
 		movement = movement.normalized * speed * Time.deltaTime;
+		//Debug.Log(movement);
 		playerRigidbody.MovePosition(transform.position + movement);
+		//playerRigidbody.position = new Vector3(playerRigidbody.position.x,0f,playerRigidbody.position.z);
 	}
 
-	void Turning(){
+	void Turning(float h, float v){
+		var angleH = ((h * 90F * Time.deltaTime) * 100f);
+		var angleV = ((v * 90F * Time.deltaTime) * 100f);
+		Debug.Log("Horizontal:"+h+"  Vertical"+v);
+		r = (angleH + angleV / 2f);
+		switch((int)h){
+			case 1:
+			  Debug.Log("DIREITA");	
+              Rotation(v,r,90f);
+              break;
+			case -1:
+			  Debug.Log("ESQUERDA");
+              Rotation(v,r,-90f);
+              break;
+			case 0:
+              Rotation(v,r,0f);
+              break;    
+		}
+		
+	}
 
+	void Rotation(float v,float r, float limit=0f){
+		switch((int)v){
+			case 1:
+			    Debug.Log("CIMA");
+				if(limit == 0f){
+					limit = 0f;
+				}else{
+					limit = limit / 2f;
+				}
+				break;
+			case -1:
+			    Debug.Log("BAIXO");
+				if(limit == 0f){
+					limit = 180f;
+				}else{
+					limit = (limit + (limit / 2));	
+				}	
+				break;
+			case 0:
+				limit = limit;
+				break;	
+		}
+		if(playerRigidbody.rotation.eulerAngles.y < limit){
+			rotation.Set(0f,r,0f);
+			Quaternion deltaRotation = Quaternion.Euler(rotation  * Time.deltaTime);
+        	playerRigidbody.MoveRotation(playerRigidbody.rotation * deltaRotation);
+		}
+	
 	}
 }
